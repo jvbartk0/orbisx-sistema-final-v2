ecf332901ce06c548917c6adcdc1ce9ef32803b3
João Vitor Bartko dos Santos <joao180507@edu.unipar.br>
2025-07-09 09:01:08 -0300

    Update main.py

--- backend/src/main.py
+++ backend/src/main.py
@@ -61,5 +61,5 @@ def serve(path):
             return "index.html not found", 404
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
-
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
